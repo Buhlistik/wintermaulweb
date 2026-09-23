c54e9eabcdf7dd71191580f55c3793edcba6c9b2
Buhlistik <131494938+Buhlistik@users.noreply.github.com>
2026-09-22 20:42:16 -0400

    Simplify tower upgrade cards for v0.85

--- multiplayer-lobby.js
+++ multiplayer-lobby.js
@@ -3,7 +3,7 @@
 
     const MAX_PLAYERS=9;
     const CONNECT_TIMEOUT_MS=6000;
-    const PROTOCOL_VERSION='0.84.0';
+    const PROTOCOL_VERSION='0.85.0';
     const MAX_CHAT_LENGTH=200;
     const SESSION_KEY='wintermaul:multiplayer-session';
     const sessionToken=(()=>{
